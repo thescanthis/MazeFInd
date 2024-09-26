596dfddfa489c3065dbf1fb3a805f98fd396ea81
thescanthis <thescanthis@gmail.com>
2024-09-26 19:42:42 +0900

    Vector

--- Algorithms/Algorithm/Algorithm.cpp
+++ Algorithms/Algorithm/Algorithm.cpp
@@ -1,20 +1,89 @@
-﻿// Algorithm.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
+using namespace std;
 
-#include <iostream>
+template<typename T>
+class Vector
+{
+public:
+	Vector()
+	{
+
+	}
+	~Vector()
+	{
+		if (_data)
+			delete[] _data;
+	}
+
+	void push_back(int value)
+	{
+		if (_size == _capacity)
+		{
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+			if (newCapacity == _capacity) newCapacity++;
+
+			reserve(newCapacity);
+		}
+
+		_data[_size] = value;
+
+		//데이터갯수 증가
+		_size++;
+	}
+
+	void reserve(int capacity)
+	{
+		if (_capacity >= capacity)
+			return;
+
+		_capacity = capacity;
+
+		T* newData = new T[_capacity];
+
+		for (int i = 0; i < _size; i++)
+			newData[i] = _data[i];
+
+		if (_data)
+			delete[] _data;
+
+		_data = newData;
+	}
+
+	T& operator[](const int pos) { return _data[pos]; }
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+	
+	void clear()
+	{
+		if (_data)
+		{
+			delete[] _data;
+			_data = new T[_capacity];
+		}
+
+		_size = 0;
+	}
+
+private:
+	T*		_data = nullptr;
+	int		_size = 0;
+	int		_capacity = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Vector<int> v;
+	v.reserve(100);
+
+	cout << v.size() << " " << v.capacity() << '\n';
+	for (int i = 0; i < 100; i++)
+	{
+		v.push_back(i);
+		cout << v[i] << " " << v.size() << " " << v.capacity() << '\n';
+	}
+
+	v.clear();
+	cout<< v.size() << " " << v.capacity() << '\n';
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
